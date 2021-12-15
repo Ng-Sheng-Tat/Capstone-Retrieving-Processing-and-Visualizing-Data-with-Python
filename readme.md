b3baa42431d3ae1924d0d321ea8792d5a5224520
Ng Sheng Tat <shengtatng@gmail.com>
2021-12-15 20:49:43 +0800

    Completion of Basics File

---
+++ readme.md
@@ -0,0 +1,3 @@
+##  Capstone Retrieving, Processing, and Visualizing Data with Python
+##  Specialization Course 5 - Python For Everybody
+##  University of Michigan (Coursera)
---
+++ review.py
@@ -0,0 +1,10 @@
+# print(ord("G"))
+# print(ord("l"))
+# print(ord("i"))
+# print(ord("s"))
+# print(ord("t"))
+
+total = 0
+for abc in range(5):
+    total = total + abc
+print(total)
